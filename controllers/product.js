527cf9117207a30ed41f18e294fcec3a17001177
Michaela DeCamara <michaela.decamara@gmail.com>
2020-04-19 12:53:41 -0400

    backend up to product done

---
+++ controllers/product.js
@@ -0,0 +1,39 @@
+const Product = require("../models/product");
+const formidable = require("formidable");
+const fs = require("fs");
+const _ = require("lodash");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+      });
+    }
+    let product = new Product(fields);
+
+    if (files.photo) {
+      console.log('FILES PHOTO: ', files.photo)
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+// sell/arrival
+// by sell = /products?sortBy=sold&order=desc&limit=4
+// by arrival = /products?sortBy=createdAt&order=desc&limit=4
+// if no params are sent, then all products are returned
+
+
