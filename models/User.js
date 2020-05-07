28b9db400fb6daa3007598366d489d050fbee316
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-07 17:46:50 -0400

    user commented

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const crypto = require("crypto");
-const uuidv1 = require("uuid/v1");
+// const crypto = require("crypto");
+// const uuidv1 = require("uuid/v1");
 
 const userSchema = new mongoose.Schema(
     {
@@ -38,33 +38,33 @@ const userSchema = new mongoose.Schema(
 );
 
 // virtual field
-userSchema
-    .virtual("password")
-    .set(function(password) {
-        this._password = password;
-        this.salt = uuidv1();
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+// userSchema
+//     .virtual("password")
+//     .set(function(password) {
+//         this._password = password;
+//         this.salt = uuidv1();
+//         this.hashed_password = this.encryptPassword(password);
+//     })
+//     .get(function() {
+//         return this._password;
+//     });
 
-userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
+// userSchema.methods = {
+//     authenticate: function(plainText) {
+//         return this.encryptPassword(plainText) === this.hashed_password;
+//     },
 
-    encryptPassword: function(password) {
-        if (!password) return "";
-        try {
-            return crypto
-                .createHmac("sha1", this.salt)
-                .update(password)
-                .digest("hex");
-        } catch (err) {
-            return "";
-        }
-    }
-};
+//     encryptPassword: function(password) {
+//         if (!password) return "";
+//         try {
+//             return crypto
+//                 .createHmac("sha1", this.salt)
+//                 .update(password)
+//                 .digest("hex");
+//         } catch (err) {
+//             return "";
+//         }
+//     }
+// };
 
 module.exports = mongoose.model("User", userSchema);
